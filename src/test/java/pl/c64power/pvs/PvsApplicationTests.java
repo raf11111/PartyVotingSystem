3653c4746ea27742b0316077504165fbcfdba682
raf <raf@localhost>
2017-07-15 12:28:26 +0200

    https://start.spring.io/

---
+++ src/test/java/pl/c64power/pvs/PvsApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.c64power.pvs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PvsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
